df45080780d8fcd555454b643c71b37249580366
anthonyb <anthony@slatestudio.com>
2015-09-08 01:06:30 -0500

    Change socket to var

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -1,6 +1,10 @@
 module.exports = function(app) {
   app.use('/list/:group_id?', function(req, res) {
     console.log(req.params.group_id)
-    res.render('list', { group_id: req.params.group_id});
+    res.render('list', {
+        group_id: req.params.group_id,
+        server: app.get('url')
+      }
+    );
   });
 }
